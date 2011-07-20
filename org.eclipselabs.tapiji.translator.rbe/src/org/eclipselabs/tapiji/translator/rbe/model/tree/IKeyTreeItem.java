b6145ff411edfdd4fe253743c393c3db6ac5241e
Martin Reiterer <martin.reiterer@inso.tuwien.ac.at>
2011-07-20 13:10:56 +0000

    Fixes minor bugs

--- org.eclipselabs.tapiji.translator.rbe/src/org/eclipselabs/tapiji/translator/rbe/model/tree/IKeyTreeItem.java
+++ org.eclipselabs.tapiji.translator.rbe/src/org/eclipselabs/tapiji/translator/rbe/model/tree/IKeyTreeItem.java
@@ -5,18 +5,52 @@ import java.util.Set;
 
 public interface IKeyTreeItem {
 
-	String getId();
+    /**
+     * Returns the key of the corresponding Resource-Bundle entry.
+     * @return The key of the Resource-Bundle entry
+     */
+    String getId();
 
-	Set<IKeyTreeItem> getChildren();
+    /**
+     * Returns the set of Resource-Bundle entries of the next deeper 
+     * hierarchy level that share the represented entry as their common
+     * parent.
+     * @return The direct child Resource-Bundle entries
+     */
+    Set<IKeyTreeItem> getChildren();
 
-	String getName();
+    /**
+     * The represented Resource-Bundle entry's id without the prefix defined
+     * by the entry's parent. 
+     * @return The Resource-Bundle entry's display name.
+     */
+    String getName();
 
-	Collection<? extends IKeyTreeItem> getNestedChildren();
+    /** 
+     * Returns the set of Resource-Bundle entries from all deeper hierarchy 
+     * levels that share the represented entry as their common parent.
+     * @return All child Resource-Bundle entries
+     */
+    Collection<? extends IKeyTreeItem> getNestedChildren();
 
-	boolean applyFilter(String filter);
+    /**
+     * Returns whether this Resource-Bundle entry is visible under the 
+     * given filter expression.
+     * @param filter The filter expression
+     * @return True if the filter expression matches the represented Resource-Bundle entry
+     */
+    boolean applyFilter(String filter);
 
-	Object getParent();
+    /**
+     * The Resource-Bundle entries parent.
+     * @return The parent Resource-Bundle entry
+     */
+    Object getParent();
 
-	IKeyTree getKeyTree();
+    /**
+     * The Resource-Bundles key representation.
+     * @return The Resource-Bundle reference, if known
+     */
+    IKeyTree getKeyTree();
 
 }
