84db103b6f7e6dcca997a34175a4891f1fa18d51
Martin Reiterer <martin.reiterer.MR@gmail.com>
2012-03-23 07:29:13 +0000

    Prototypes marker update approach.
    Gets the markers current position information from the abstract marker annotation model.

--- org.eclipselabs.tapiji.tools.core/src/org/eclipselabs/tapiji/tools/core/builder/ViolationResolutionGenerator.java
+++ org.eclipselabs.tapiji.tools.core/src/org/eclipselabs/tapiji/tools/core/builder/ViolationResolutionGenerator.java
@@ -8,26 +8,30 @@ import org.eclipse.ui.IMarkerResolutionGenerator2;
 import org.eclipselabs.tapiji.tools.core.extensions.I18nAuditor;
 import org.eclipselabs.tapiji.tools.core.model.exception.NoSuchResourceAuditorException;
 
+public class ViolationResolutionGenerator implements
+	IMarkerResolutionGenerator2 {
 
-public class ViolationResolutionGenerator implements IMarkerResolutionGenerator2 {
+    @Override
+    public boolean hasResolutions(IMarker marker) {
+	return true;
+    }
 
-	@Override
-	public boolean hasResolutions(IMarker marker) {
-		return true;
+    @Override
+    public IMarkerResolution[] getResolutions(IMarker marker) {
+	String contextId = marker.getAttribute("context", "");
+
+	// find resolution generator for the given context
+	try {
+	    I18nAuditor auditor = StringLiteralAuditor
+		    .getI18nAuditorByContext(contextId);
+	    List<IMarkerResolution> resolutions = auditor
+		    .getMarkerResolutions(marker);
+	    return resolutions
+		    .toArray(new IMarkerResolution[resolutions.size()]);
+	} catch (NoSuchResourceAuditorException e) {
 	}
 
-	@Override
-	public IMarkerResolution[] getResolutions(IMarker marker) {
-		String contextId = marker.getAttribute("context", "");
-		
-		// find resolution generator for the given context
-		try {
-			I18nAuditor auditor = StringLiteralAuditor.getI18nAuditorByContext(contextId);
-			List<IMarkerResolution> resolutions = auditor.getMarkerResolutions(marker);
-			return resolutions.toArray(new IMarkerResolution[resolutions.size()]);
-		} catch (NoSuchResourceAuditorException e) {}
-		
-		return new IMarkerResolution[0]; 
-	}
+	return new IMarkerResolution[0];
+    }
 
 }
