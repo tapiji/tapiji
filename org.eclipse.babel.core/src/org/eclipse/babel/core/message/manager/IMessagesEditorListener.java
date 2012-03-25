933ef8777cd8d916dd476da552eb5f426640e985
Alexej Strelzow <alexej.strelzow@gmail.com>
2012-03-25 13:31:46 +0000

    fixes issue 37
    IMessagesEditorListener got a new method, which tells the listener which resource has been changed.
    Now it is possible for the I18NPage to know which key it has to update. Prior to this change, the update was triggered by a change of the selecion of the key in the key-tree-view of the editor.

--- org.eclipse.babel.core/src/org/eclipse/babel/core/message/manager/IMessagesEditorListener.java
+++ org.eclipse.babel.core/src/org/eclipse/babel/core/message/manager/IMessagesEditorListener.java
@@ -1,9 +1,13 @@
 package org.eclipse.babel.core.message.manager;
 
+import org.eclipselabs.tapiji.translator.rbe.babel.bundle.IMessagesBundle;
+
 public interface IMessagesEditorListener {
 
 	void onSave();
 	
 	void onModify();
+	
+	void onResourceChanged(IMessagesBundle bundle);
 	
 }
