6fd08d88f3724a73261ed91defe3c1881699fe9e
Martin Reiterer <martin.reiterer.MR@gmail.com>
2012-04-04 17:29:50 +0000

    Removes the unused completion proposal `ValueOfResourceBundleProposal`.

--- org.eclipse.babel.tapiji.tools.java/src/org/eclipse/babel/tapiji/tools/java/ui/MessageCompletionProposalComputer.java
+++ org.eclipse.babel.tapiji.tools.java/src/org/eclipse/babel/tapiji/tools/java/ui/MessageCompletionProposalComputer.java
@@ -28,189 +28,204 @@ import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 
-
 public class MessageCompletionProposalComputer implements
-		IJavaCompletionProposalComputer {
+	IJavaCompletionProposalComputer {
 
-	private ResourceAuditVisitor csav;
-	private IJavaElement je;
-	private IJavaElement javaElement;
-	private CompilationUnit cu;
-	
-	public MessageCompletionProposalComputer() {
-		
-	}
+    private ResourceAuditVisitor csav;
+    private IJavaElement je;
+    private IJavaElement javaElement;
+    private CompilationUnit cu;
 
-	
-	
-	@Override
-	public List<ICompletionProposal> computeCompletionProposals(
-			ContentAssistInvocationContext context, IProgressMonitor monitor) {
-		List<ICompletionProposal> completions = new ArrayList<ICompletionProposal>();
-		
-		if (!InternationalizationNature.hasNature(((JavaContentAssistInvocationContext) context)
-				.getCompilationUnit().getResource().getProject()) )
-			return completions;
-		
-		try {
-			CompletionContext coreContext = ((JavaContentAssistInvocationContext) context)
-					.getCoreContext();
-			int offset = coreContext.getOffset();
-			
-			if (javaElement == null)
-				javaElement = ((JavaContentAssistInvocationContext) context)
-								.getCompilationUnit().getElementAt(offset);
-			
-			String stringLiteralStart = new String(coreContext.getToken());
+    public MessageCompletionProposalComputer() {
 
-			// Check if the string literal is up to be written within the context
-			// of a resource-bundle accessor method
-			ResourceBundleManager manager = ResourceBundleManager.getManager(javaElement.getResource().getProject());
+    }
 
-			if (csav == null) {
-				csav = new ResourceAuditVisitor(null, 
-						manager);
-				je = JavaCore.create(javaElement.getResource());
-			}	
+    @Override
+    public List<ICompletionProposal> computeCompletionProposals(
+	    ContentAssistInvocationContext context, IProgressMonitor monitor) {
+	List<ICompletionProposal> completions = new ArrayList<ICompletionProposal>();
 
-			if (je instanceof ICompilationUnit) {
-				// get the type of the currently loaded resource
-				if (cu == null) {
-					ITypeRoot typeRoot = ((ICompilationUnit) je);
-					
-					if (typeRoot == null)
-						return null;
-					
-					cu = ASTutils.getCompilationUnit(typeRoot);
-					
-					cu.accept(csav);
-				}
-			
-				//StringLiteralAnalyzer sla = new StringLiteralAnalyzer(javaElement.getResource());
-				if (csav.getKeyAt(new Long(offset)) != null) {	
-					completions.addAll(getResourceBundleCompletionProposals(offset, stringLiteralStart, manager, csav, javaElement.getResource()));
-				} else if (csav.getRBReferenceAt(new Long(offset)) != null) {
-					completions.addAll(getRBReferenceCompletionProposals(offset, stringLiteralStart, manager, je.getResource()));
-				} else {
-					completions.addAll(getBasicJavaCompletionProposals(offset, stringLiteralStart, manager, csav, je.getResource()));
-				}
-				if (completions.size() == 1)
-					completions.add(new NoActionProposal());
-			} else
-				return null;
-		} catch (Exception e) {
-			// e.printStackTrace();
+	if (!InternationalizationNature
+		.hasNature(((JavaContentAssistInvocationContext) context)
+			.getCompilationUnit().getResource().getProject()))
+	    return completions;
+
+	try {
+	    CompletionContext coreContext = ((JavaContentAssistInvocationContext) context)
+		    .getCoreContext();
+	    int offset = coreContext.getOffset();
+
+	    if (javaElement == null)
+		javaElement = ((JavaContentAssistInvocationContext) context)
+			.getCompilationUnit().getElementAt(offset);
+
+	    String stringLiteralStart = new String(coreContext.getToken());
+
+	    // Check if the string literal is up to be written within the
+	    // context
+	    // of a resource-bundle accessor method
+	    ResourceBundleManager manager = ResourceBundleManager
+		    .getManager(javaElement.getResource().getProject());
+
+	    if (csav == null) {
+		csav = new ResourceAuditVisitor(null, manager);
+		je = JavaCore.create(javaElement.getResource());
+	    }
+
+	    if (je instanceof ICompilationUnit) {
+		// get the type of the currently loaded resource
+		if (cu == null) {
+		    ITypeRoot typeRoot = ((ICompilationUnit) je);
+
+		    if (typeRoot == null)
+			return null;
+
+		    cu = ASTutils.getCompilationUnit(typeRoot);
+
+		    cu.accept(csav);
 		}
-		return completions;
-	}
-	
-	private Collection<ICompletionProposal> getRBReferenceCompletionProposals(
-			int offset, String stringLiteralStart, ResourceBundleManager manager, IResource resource) {
-		List<ICompletionProposal> completions = new ArrayList<ICompletionProposal>();
-		int posStart = offset - stringLiteralStart.length();
-		boolean hit = false;
-		
-		// Show a list of available resource bundles
-		List<String> resourceBundles = manager.getResourceBundleIdentifiers();
-		for (String rbName : resourceBundles) {
-			if (rbName.startsWith(stringLiteralStart)) {
-				if (rbName.equals(stringLiteralStart))
-					hit = true;
-				else
-					completions.add (new MessageCompletionProposal (posStart, stringLiteralStart.length(), rbName, true));	
-			}
-		}
-		
-		if (!hit && stringLiteralStart.trim().length() > 0)
-			completions.add(new CreateResourceBundleProposal(stringLiteralStart, resource, posStart, posStart + stringLiteralStart.length()));
-		
-		return completions;
-	}
 
-	protected List<ICompletionProposal> getBasicJavaCompletionProposals (
-			int offset, String stringLiteralStart, 
-			ResourceBundleManager manager, ResourceAuditVisitor csav, IResource resource) {
-		List<ICompletionProposal> completions = new ArrayList<ICompletionProposal>();
-		
-		if (stringLiteralStart.length() == 0) {
-			// If nothing has been entered
-			completions.add(new InsertResourceBundleReferenceProposal(
-					offset - stringLiteralStart.length(),
-					stringLiteralStart.length(), 
-					manager, resource, csav.getDefinedResourceBundles(offset)));
-			completions.add(new NewResourceBundleEntryProposal(resource, offset - stringLiteralStart.length(),
-					false, manager, null/*, csav.getDefinedResourceBundles(offset)*/));
+		// StringLiteralAnalyzer sla = new
+		// StringLiteralAnalyzer(javaElement.getResource());
+		if (csav.getKeyAt(new Long(offset)) != null) {
+		    completions.addAll(getResourceBundleCompletionProposals(
+			    offset, stringLiteralStart, manager, csav,
+			    javaElement.getResource()));
+		} else if (csav.getRBReferenceAt(new Long(offset)) != null) {
+		    completions.addAll(getRBReferenceCompletionProposals(
+			    offset, stringLiteralStart, manager,
+			    je.getResource()));
 		} else {
-//			if (!"Hallo Welt".equals(stringLiteralStart)) {
-//				// If a part of a String has already been entered
-//				if ("Hallo Welt".startsWith(stringLiteralStart) ) {
-//					completions.add(new ValueOfResourceBundleProposal(offset - stringLiteralStart.length(),
-//							stringLiteralStart.length(), "hello.world", "Hallo Welt!"));
-//				}
-				completions.add(new NewResourceBundleEntryProposal(resource, offset - stringLiteralStart.length(),
-						false, manager, null/*, csav.getDefinedResourceBundles(offset)*/));
-			
-//			}
+		    completions
+			    .addAll(getBasicJavaCompletionProposals(offset,
+				    stringLiteralStart, manager, csav,
+				    je.getResource()));
 		}
-		return completions;
-	}
-	
-	protected List<ICompletionProposal> getResourceBundleCompletionProposals (
-			int offset, String stringLiteralStart, ResourceBundleManager manager,
-			ResourceAuditVisitor csav, IResource resource) {
-		List<ICompletionProposal> completions = new ArrayList<ICompletionProposal>();
-		IRegion region = csav.getKeyAt(new Long(offset));
-		String bundleName = csav.getBundleReference(region);
-		int posStart = offset - stringLiteralStart.length();
-		IMessagesBundleGroup bundleGroup = manager.getResourceBundle(bundleName);
-		
-		if (stringLiteralStart.length() > 0) {
-			boolean hit = false;
-			// If a part of a String has already been entered	
-			for (String key : bundleGroup.getMessageKeys()) {
-				if (key.toLowerCase().startsWith(stringLiteralStart.toLowerCase())) {
-					if (!key.equals(stringLiteralStart))
-						completions.add(new MessageCompletionProposal(posStart, 
-							stringLiteralStart.length(), key, false));
-					else
-						hit = true;
-				}
-			}
-			if (!hit)
-				completions.add(new NewResourceBundleEntryProposal(resource, offset - stringLiteralStart.length(),
-						true, manager, bundleName/*, csav.getDefinedResourceBundles(offset)*/));
-		} else {
-			for (String key : bundleGroup.getMessageKeys()) {
-				completions.add (new MessageCompletionProposal (posStart, stringLiteralStart.length(), key, false));
-			}
-			completions.add(new NewResourceBundleEntryProposal(resource, offset - stringLiteralStart.length(),
-					true, manager, bundleName/*, csav.getDefinedResourceBundles(offset)*/));
-			
-		}
-		return completions;
-	}
-
-	@Override
-	public List computeContextInformation(
-			ContentAssistInvocationContext context, IProgressMonitor monitor) {
+		if (completions.size() == 1)
+		    completions.add(new NoActionProposal());
+	    } else
 		return null;
+	} catch (Exception e) {
+	    // e.printStackTrace();
+	}
+	return completions;
+    }
+
+    private Collection<ICompletionProposal> getRBReferenceCompletionProposals(
+	    int offset, String stringLiteralStart,
+	    ResourceBundleManager manager, IResource resource) {
+	List<ICompletionProposal> completions = new ArrayList<ICompletionProposal>();
+	int posStart = offset - stringLiteralStart.length();
+	boolean hit = false;
+
+	// Show a list of available resource bundles
+	List<String> resourceBundles = manager.getResourceBundleIdentifiers();
+	for (String rbName : resourceBundles) {
+	    if (rbName.startsWith(stringLiteralStart)) {
+		if (rbName.equals(stringLiteralStart))
+		    hit = true;
+		else
+		    completions.add(new MessageCompletionProposal(posStart,
+			    stringLiteralStart.length(), rbName, true));
+	    }
 	}
 
-	@Override
-	public String getErrorMessage() {
-		// TODO Auto-generated method stub
-		return "";
-	}
+	if (!hit && stringLiteralStart.trim().length() > 0)
+	    completions.add(new CreateResourceBundleProposal(
+		    stringLiteralStart, resource, posStart, posStart
+			    + stringLiteralStart.length()));
 
-	@Override
-	public void sessionEnded() {
-		csav = null;
-		javaElement = null;
-		cu = null;
-	}
+	return completions;
+    }
 
-	@Override
-	public void sessionStarted() {
+    protected List<ICompletionProposal> getBasicJavaCompletionProposals(
+	    int offset, String stringLiteralStart,
+	    ResourceBundleManager manager, ResourceAuditVisitor csav,
+	    IResource resource) {
+	List<ICompletionProposal> completions = new ArrayList<ICompletionProposal>();
+
+	if (stringLiteralStart.length() == 0) {
+	    // If nothing has been entered
+	    completions.add(new InsertResourceBundleReferenceProposal(offset
+		    - stringLiteralStart.length(), stringLiteralStart.length(),
+		    manager, resource, csav.getDefinedResourceBundles(offset)));
+	    completions.add(new NewResourceBundleEntryProposal(resource, offset
+		    - stringLiteralStart.length(), false, manager, null));
+	} else {
+	    completions.add(new NewResourceBundleEntryProposal(resource, offset
+		    - stringLiteralStart.length(), false, manager, null));
 	}
+	return completions;
+    }
+
+    protected List<ICompletionProposal> getResourceBundleCompletionProposals(
+	    int offset, String stringLiteralStart,
+	    ResourceBundleManager manager, ResourceAuditVisitor csav,
+	    IResource resource) {
+	List<ICompletionProposal> completions = new ArrayList<ICompletionProposal>();
+	IRegion region = csav.getKeyAt(new Long(offset));
+	String bundleName = csav.getBundleReference(region);
+	int posStart = offset - stringLiteralStart.length();
+	IMessagesBundleGroup bundleGroup = manager
+		.getResourceBundle(bundleName);
+
+	if (stringLiteralStart.length() > 0) {
+	    boolean hit = false;
+	    // If a part of a String has already been entered
+	    for (String key : bundleGroup.getMessageKeys()) {
+		if (key.toLowerCase().startsWith(
+			stringLiteralStart.toLowerCase())) {
+		    if (!key.equals(stringLiteralStart))
+			completions.add(new MessageCompletionProposal(posStart,
+				stringLiteralStart.length(), key, false));
+		    else
+			hit = true;
+		}
+	    }
+	    if (!hit)
+		completions.add(new NewResourceBundleEntryProposal(resource,
+			offset - stringLiteralStart.length(), true, manager,
+			bundleName/*
+				   * , csav.getDefinedResourceBundles (offset)
+				   */));
+	} else {
+	    for (String key : bundleGroup.getMessageKeys()) {
+		completions.add(new MessageCompletionProposal(posStart,
+			stringLiteralStart.length(), key, false));
+	    }
+	    completions
+		    .add(new NewResourceBundleEntryProposal(resource, offset
+			    - stringLiteralStart.length(), true, manager,
+			    bundleName/*
+				       * ,
+				       * csav.getDefinedResourceBundles(offset)
+				       */));
+
+	}
+	return completions;
+    }
+
+    @Override
+    public List computeContextInformation(
+	    ContentAssistInvocationContext context, IProgressMonitor monitor) {
+	return null;
+    }
+
+    @Override
+    public String getErrorMessage() {
+	// TODO Auto-generated method stub
+	return "";
+    }
+
+    @Override
+    public void sessionEnded() {
+	csav = null;
+	javaElement = null;
+	cu = null;
+    }
+
+    @Override
+    public void sessionStarted() {
+    }
 
 }
--- org.eclipse.babel.tapiji.tools.java/src/org/eclipse/babel/tapiji/tools/java/ui/autocompletion/Sorter.java
+++ org.eclipse.babel.tapiji.tools.java/src/org/eclipse/babel/tapiji/tools/java/ui/autocompletion/Sorter.java
@@ -6,36 +6,35 @@ import org.eclipse.jface.text.contentassist.ICompletionProposal;
 
 public class Sorter extends AbstractProposalSorter {
 
-	private boolean loaded = false;
-	
-	public Sorter () {
-		// i18n
-		loaded = true;
-	}
-	
-	@Override
-	public void beginSorting(ContentAssistInvocationContext context) {
-		// TODO Auto-generated method stub
-		super.beginSorting(context);
-	}
-	
-	@Override
-	public int compare(ICompletionProposal prop1, ICompletionProposal prop2) {
-		return getIndex (prop1) - getIndex (prop2);
-	}
+    private boolean loaded = false;
 
-	protected int getIndex (ICompletionProposal prop) {
-		if (prop instanceof NoActionProposal)
-			return 1;
-		else if (prop instanceof MessageCompletionProposal)
-			return 2;
-		else if (prop instanceof ValueOfResourceBundleProposal)
-			return 3;
-		else if (prop instanceof InsertResourceBundleReferenceProposal)
-			return 4;
-		else if (prop instanceof NewResourceBundleEntryProposal)
-			return 5;
-		else return 0;
-	}
-	
+    public Sorter() {
+	// i18n
+	loaded = true;
+    }
+
+    @Override
+    public void beginSorting(ContentAssistInvocationContext context) {
+	// TODO Auto-generated method stub
+	super.beginSorting(context);
+    }
+
+    @Override
+    public int compare(ICompletionProposal prop1, ICompletionProposal prop2) {
+	return getIndex(prop1) - getIndex(prop2);
+    }
+
+    protected int getIndex(ICompletionProposal prop) {
+	if (prop instanceof NoActionProposal)
+	    return 1;
+	else if (prop instanceof MessageCompletionProposal)
+	    return 2;
+	else if (prop instanceof InsertResourceBundleReferenceProposal)
+	    return 3;
+	else if (prop instanceof NewResourceBundleEntryProposal)
+	    return 4;
+	else
+	    return 0;
+    }
+
 }
