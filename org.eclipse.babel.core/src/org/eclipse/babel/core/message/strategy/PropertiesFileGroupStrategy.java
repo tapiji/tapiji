62b67514d464f47663cf4c2d6dd7e1abff71eef0
Alexej Strelzow <alexej.strelzow@gmail.com>
2012-04-27 13:26:22 +0000

    contributes to Issue 73:
    documentation & license header

--- org.eclipse.babel.core/src/org/eclipse/babel/core/message/strategy/PropertiesFileGroupStrategy.java
+++ org.eclipse.babel.core/src/org/eclipse/babel/core/message/strategy/PropertiesFileGroupStrategy.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Pascal Essiembre - initial API and implementation
+ *    Alexej Strelzow - TapJI integration, messagesBundleId
  ******************************************************************************/
 package org.eclipse.babel.core.message.strategy;
 
