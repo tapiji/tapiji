e414a6b67730b795af9c0c05385764e116a50131
Alexej Strelzow <alexej.strelzow@gmail.com>
2012-04-27 13:22:53 +0000

    contributes to Issue 73:
    changes, because of: seperation of ui/non-ui, documentation

--- org.eclipse.babel.tapiji.tools.java.ui/src/org/eclipse/babel/tapiji/tools/java/ui/util/ASTutilsUI.java
+++ org.eclipse.babel.tapiji.tools.java.ui/src/org/eclipse/babel/tapiji/tools/java/ui/util/ASTutilsUI.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2012 TapiJI.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  * Contributors:
+ * 	   Martin Reiterer - initial API and implementation
+ *     Alexej Strelzow - seperation of ui/non-ui (methods moved from ASTUtils)
+ ******************************************************************************/
+
 package org.eclipse.babel.tapiji.tools.java.ui.util;
 
 import java.util.Locale;
--- org.eclipse.babel.tapiji.tools.java/src/org/eclipse/babel/tapiji/tools/java/util/ASTutils.java
+++ org.eclipse.babel.tapiji.tools.java/src/org/eclipse/babel/tapiji/tools/java/util/ASTutils.java
@@ -4,6 +4,10 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  * Contributors:
+ * 	   Martin Reiterer - initial API and implementation
+ *     Alexej Strelzow - seperation of ui/non-ui
  ******************************************************************************/
 package org.eclipse.babel.tapiji.tools.java.util;
 
