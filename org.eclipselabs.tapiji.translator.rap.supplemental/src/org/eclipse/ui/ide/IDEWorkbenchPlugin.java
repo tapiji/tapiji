413f303ec3417d92f6fc1ac672c92f83a7c322fc
Matthias Lettmayer <e0728179@student.tuwien.ac.at>
2012-07-05 15:00:22 +0000

    [RAP] removed compile error from IDE, uncommented unused parts

---
+++ org.eclipselabs.tapiji.translator.rap.supplemental/src/org/eclipse/ui/ide/IDEWorkbenchPlugin.java
@@ -0,0 +1,17 @@
+package org.eclipse.ui.ide;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * StyledCellLabelProvider: added only needed parts, just to get the source code compiled in RAP.
+ * 
+ * @author Matthias Lettmayer
+ *
+ */
+public class IDEWorkbenchPlugin extends AbstractUIPlugin {
+	/**
+     * The ID of the default text editor.
+     * This must correspond to EditorsUI.DEFAULT_TEXT_EDITOR_ID.
+     */
+    public static final String DEFAULT_TEXT_EDITOR_ID = "org.eclipse.ui.DefaultTextEditor"; //$NON-NLS-1$
+}
