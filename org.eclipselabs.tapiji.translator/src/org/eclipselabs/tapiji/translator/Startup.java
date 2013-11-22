3c26840439331864822aa082fecffed0705e3290
samir soyer <samirsoyer@gmail.com>
2013-11-23 00:56:03 +0100

    Completion of Javadoc in own classes

--- org.eclipselabs.tapiji.translator/src/org/eclipselabs/tapiji/translator/Startup.java
+++ org.eclipselabs.tapiji.translator/src/org/eclipselabs/tapiji/translator/Startup.java
@@ -3,8 +3,17 @@ package org.eclipselabs.tapiji.translator;
 import org.eclipse.ui.IStartup;
 import org.eclipselabs.tapiji.translator.suggestionlookup.SuggestionProviderLoader;
 
+/**
+ * Class that contains the method to call 
+ * after startup
+ * @author Samir Soyer
+ *
+ */
 public class Startup implements IStartup{
 
+	/**
+	 * @see org.eclipse.ui.IStartup#earlyStartup()
+	 */
 	@Override
 	public void earlyStartup() {
 		SuggestionProviderLoader.registerProviders();
--- org.eclipselabs.tapiji.translator/src/org/eclipselabs/tapiji/translator/suggestionlookup/SuggestionProviderLoader.java
+++ org.eclipselabs.tapiji.translator/src/org/eclipselabs/tapiji/translator/suggestionlookup/SuggestionProviderLoader.java
@@ -9,10 +9,23 @@ import org.eclipse.core.runtime.ISafeRunnable;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.SafeRunner;
 
+/**
+ * Look up for suggestion providers, which implements the extension point
+ * {@literal "org.eclipselabs.tapiji.translator.suggestion"}.
+ * @author Samir Soyer
+ *
+ */
 public class SuggestionProviderLoader {
 	private static final String ISUGGESTIONPROVIDER_ID = 
 			"org.eclipselabs.tapiji.translator.suggestion";
 
+	/**
+	 * Finds all the suggestion providers resp. extensions that implement
+	 * {@code org.eclipselabs.tapiji.translator.suggestion} extension point.
+	 * Then registers them at 
+	 * {@link org.eclipse.babel.editor.widgets.
+	 * suggestion.provider.SuggestionProviderUtils}
+	 */
 	public static void registerProviders() {
 		IExtensionRegistry registry = Platform.getExtensionRegistry();
 
