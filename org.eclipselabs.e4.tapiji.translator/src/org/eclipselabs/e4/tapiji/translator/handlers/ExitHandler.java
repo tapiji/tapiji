276405e7e23beadebde7662d972865b55561138e
Christian Behon <christian.behon@gmail.com>
2015-03-03 20:36:52 +0100

    fixes dependencies errors

--- org.eclipselabs.e4.tapiji.translator/src/org/eclipselabs/e4/tapiji/translator/handlers/ExitHandler.java
+++ org.eclipselabs.e4.tapiji.translator/src/org/eclipselabs/e4/tapiji/translator/handlers/ExitHandler.java
@@ -1,7 +1,13 @@
 package org.eclipselabs.e4.tapiji.translator.handlers;
 
 
+import javax.inject.Named;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 
 public class ExitHandler {
@@ -9,9 +15,11 @@ public class ExitHandler {
   private static final String TAG = ExitHandler.class.getSimpleName();
 
   @Execute
-  public void execute() {
+  public void execute(IWorkbench workbench, IEclipseContext context, @Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
     System.out.println("Execute: " + TAG);
-
+    if (MessageDialog.openConfirm(shell, "Confirmation", "Do you want to exit?")) {
+      workbench.close();
+    }
   }
 
 }
--- org.eclipselabs.e4.tapiji.translator/src/org/eclipselabs/e4/tapiji/translator/utils/FileUtils.java
+++ org.eclipselabs.e4.tapiji.translator/src/org/eclipselabs/e4/tapiji/translator/utils/FileUtils.java
@@ -10,8 +10,8 @@
  ******************************************************************************/
 package org.eclipselabs.e4.tapiji.translator.utils;
 
-import java.io.File;
 
+import java.io.File;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -24,149 +24,141 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
+
 public class FileUtils {
 
-	/** Token to replace in a regular expression with a bundle name. */
-	private static final String TOKEN_BUNDLE_NAME = "BUNDLENAME";
-	/** Token to replace in a regular expression with a file extension. */
-	private static final String TOKEN_FILE_EXTENSION = "FILEEXTENSION";
-	/** Regex to match a properties file. */
-	public static final String PROPERTIES_FILE_REGEX = "^("
-	        + TOKEN_BUNDLE_NAME + ")" + "((_[a-z]{2,3})|(_[a-z]{2,3}_[A-Z]{2})"
-	        + "|(_[a-z]{2,3}_[A-Z]{2}_\\w*))?(\\." + TOKEN_FILE_EXTENSION
-	        + ")$";
+  /** Token to replace in a regular expression with a bundle name. */
+  private static final String TOKEN_BUNDLE_NAME = "BUNDLENAME";
+  /** Token to replace in a regular expression with a file extension. */
+  private static final String TOKEN_FILE_EXTENSION = "FILEEXTENSION";
+  /** Regex to match a properties file. */
+  public static final String PROPERTIES_FILE_REGEX = "^(" + TOKEN_BUNDLE_NAME + ")"
+          + "((_[a-z]{2,3})|(_[a-z]{2,3}_[A-Z]{2})" + "|(_[a-z]{2,3}_[A-Z]{2}_\\w*))?(\\." + TOKEN_FILE_EXTENSION
+          + ")$";
 
-	/** Project name for external resource bundles */
-	public static final String EXTERNAL_RB_PROJECT_NAME = "ExternalResourceBundles";
-	
-	/** The singleton instance of Workspace */
-	private static IWorkspace workspace;
+  /** Project name for external resource bundles */
+  public static final String EXTERNAL_RB_PROJECT_NAME = "ExternalResourceBundles";
 
-	public static boolean isResourceBundle(String fileName) {
-		return fileName.toLowerCase().endsWith(".properties");
-	}
+  /** The singleton instance of Workspace */
+  private static IWorkspace workspace;
 
-	public static boolean isGlossary(String fileName) {
-		return fileName.toLowerCase().endsWith(".xml");
-	}
+  public static boolean isResourceBundle(String fileName) {
+    return fileName.toLowerCase().endsWith(".properties");
+  }
 
-	public static IWorkspace getWorkspace() {
-		if (workspace == null) {
-			workspace = ResourcesPlugin.getWorkspace();
-		}
+  public static boolean isGlossary(String fileName) {
+    return fileName.toLowerCase().endsWith(".xml");
+  }
 
-		return workspace;
-	}
+  public static IWorkspace getWorkspace() {
+    if (workspace == null) {
+      workspace = ResourcesPlugin.getWorkspace();
+    }
 
-	public static IProject getProject(String projectName) throws CoreException {
-		IProject project = getWorkspace().getRoot().getProject(
-			        projectName);
-		
-		if (!project.exists())
-			project.create(null);
-		if (!project.isOpen())
-			project.open(null);
+    return workspace;
+  }
 
-		return project;
-	}
+  public static IProject getProject(String projectName) throws CoreException {
+    IProject project = getWorkspace().getRoot().getProject(projectName);
 
-	public static void prePareEditorInputs() {
-		IWorkspace workspace = getWorkspace();
-	}
+    if (!project.exists())
+      project.create(null);
+    if (!project.isOpen())
+      project.open(null);
 
-	public static IFile getResourceBundleRef(String location, String projectName)
-	        throws CoreException {
-		IPath path = new Path(location);
+    return project;
+  }
 
-		/**
-		 * Create all files of the Resource-Bundle within the project space and
-		 * link them to the original file
-		 */
-		String regex = getPropertiesFileRegEx(path);
-		String projPathName = toProjectRelativePathName(path);
-		IProject project = getProject(projectName);
-		IFile file = project.getFile(projPathName);
-		file.createLink(path, IResource.REPLACE, null);
+  public static void prePareEditorInputs() {
+    IWorkspace workspace = getWorkspace();
+  }
 
-		File parentDir = new File(path.toFile().getParent());
-		String[] files = parentDir.list();
+  public static IFile getResourceBundleRef(String location, String projectName) throws CoreException {
+    IPath path = new Path(location);
 
-		for (String fn : files) {
-			File fo = new File(parentDir, fn);
-			if (!fo.isFile())
-				continue;
+    /**
+     * Create all files of the Resource-Bundle within the project space and link them to the original file
+     */
+    String regex = getPropertiesFileRegEx(path);
+    String projPathName = toProjectRelativePathName(path);
+    IProject project = getProject(projectName);
+    IFile file = project.getFile(projPathName);
+    file.createLink(path, IResource.REPLACE, null);
 
-			IPath newFilePath = new Path(fo.getAbsolutePath());
-			if (fo.getName().matches(regex)
-			        && !path.toFile().getName()
-			                .equals(newFilePath.toFile().getName())) {
-				IFile newFile = project
-				        .getFile(toProjectRelativePathName(newFilePath));
-				newFile.createLink(newFilePath, IResource.REPLACE, null);
-			}
-		}
+    File parentDir = new File(path.toFile().getParent());
+    String[] files = parentDir.list();
 
-		return file;
-	}
-	
-	protected static String toProjectRelativePathName(IPath path) {
-		String projectRelativeName = "";
+    for (String fn : files) {
+      File fo = new File(parentDir, fn);
+      if (!fo.isFile())
+        continue;
 
-		projectRelativeName = path.toString().replaceAll(":", "");
-		projectRelativeName = projectRelativeName.replaceAll("/", ".");
+      IPath newFilePath = new Path(fo.getAbsolutePath());
+      if (fo.getName().matches(regex) && !path.toFile().getName().equals(newFilePath.toFile().getName())) {
+        IFile newFile = project.getFile(toProjectRelativePathName(newFilePath));
+        newFile.createLink(newFilePath, IResource.REPLACE, null);
+      }
+    }
 
-		return projectRelativeName;
-	}
+    return file;
+  }
 
-	protected static String getPropertiesFileRegEx(IPath file) {
-		String bundleName = getBundleName(file);
-		return PROPERTIES_FILE_REGEX
-		        .replaceFirst(TOKEN_BUNDLE_NAME, bundleName).replaceFirst(
-		                TOKEN_FILE_EXTENSION, file.getFileExtension());
-	}
+  protected static String toProjectRelativePathName(IPath path) {
+    String projectRelativeName = "";
 
-	public static String getBundleName(IPath file) {
-		String name = file.toFile().getName();
-		String regex = "^(.*?)" //$NON-NLS-1$
-		        + "((_[a-z]{2,3})|(_[a-z]{2,3}_[A-Z]{2})"
-		        + "|(_[a-z]{2,3}_[A-Z]{2}_\\w*))?(\\."
-		        + file.getFileExtension() + ")$";
-		return name.replaceFirst(regex, "$1");
-	}
-	
-	public static String getBundleName(String filePath) {
-		IPath path = new Path(filePath);		
-		return getBundleName(path);
-	}
+    projectRelativeName = path.toString().replaceAll(":", "");
+    projectRelativeName = projectRelativeName.replaceAll("/", ".");
 
-	public static String[] queryFileName(Shell shell, String title,
-	        int dialogOptions, String[] endings) {
-		FileDialog dialog = new FileDialog(shell, dialogOptions);
-		dialog.setText(title);
-		dialog.setFilterExtensions(endings);
-		
-		String filepath = dialog.open();
-		
-		// if single option, return path
-		if ((dialogOptions & SWT.SINGLE) == SWT.SINGLE) {
-			return new String[] {filepath};
-		} else {
-			// [RAP] In RAP1.5 getFilterPath is always empty!!!
-			String path = dialog.getFilterPath();
-			// [RAP] In RAP1.5 getFileNames returns full filename (+ path)!!!
-			String[] filenames = dialog.getFileNames();
-	
-			// append filenames to path
-			if (! path.isEmpty()) {
-				for (int i=0; i < filenames.length; i++) {
-					filenames[i] = path + File.separator + filenames[i];
-				}
-					
-			}			
-			if (filenames.length > 0)
-				return filenames;
-			return null;
-		}
-	}
+    return projectRelativeName;
+  }
+
+  protected static String getPropertiesFileRegEx(IPath file) {
+    String bundleName = getBundleName(file);
+    return PROPERTIES_FILE_REGEX.replaceFirst(TOKEN_BUNDLE_NAME, bundleName).replaceFirst(TOKEN_FILE_EXTENSION,
+            file.getFileExtension());
+  }
+
+  public static String getBundleName(IPath file) {
+    String name = file.toFile().getName();
+    String regex = "^(.*?)" //$NON-NLS-1$
+            + "((_[a-z]{2,3})|(_[a-z]{2,3}_[A-Z]{2})" + "|(_[a-z]{2,3}_[A-Z]{2}_\\w*))?(\\."
+            + file.getFileExtension()
+            + ")$";
+    return name.replaceFirst(regex, "$1");
+  }
+
+  public static String getBundleName(String filePath) {
+    IPath path = new Path(filePath);
+    return getBundleName(path);
+  }
+
+  public static String[] queryFileName(Shell shell, String title, int dialogOptions, String[] endings) {
+    FileDialog dialog = new FileDialog(shell, dialogOptions);
+    dialog.setText(title);
+    dialog.setFilterExtensions(endings);
+
+    String filepath = dialog.open();
+
+    // if single option, return path
+    if ((dialogOptions & SWT.SINGLE) == SWT.SINGLE) {
+      return new String[] {filepath};
+    } else {
+      // [RAP] In RAP1.5 getFilterPath is always empty!!!
+      String path = dialog.getFilterPath();
+      // [RAP] In RAP1.5 getFileNames returns full filename (+ path)!!!
+      String[] filenames = dialog.getFileNames();
+
+      // append filenames to path
+      if (!path.isEmpty()) {
+        for (int i = 0; i < filenames.length; i++) {
+          filenames[i] = path + File.separator + filenames[i];
+        }
+
+      }
+      if (filenames.length > 0)
+        return filenames;
+      return null;
+    }
+  }
 
 }
