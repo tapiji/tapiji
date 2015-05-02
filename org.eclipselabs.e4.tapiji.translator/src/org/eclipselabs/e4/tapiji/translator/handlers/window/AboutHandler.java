52507f185f1bec2f4ce08ade38ad2f9c27fc20d6
Christian Behon <christian.behon@gmail.com>
2015-05-02 22:10:12 +0200

    Add about dialog

--- org.eclipselabs.e4.tapiji.translator/src/org/eclipselabs/e4/tapiji/translator/handlers/window/AboutHandler.java
+++ org.eclipselabs.e4.tapiji.translator/src/org/eclipselabs/e4/tapiji/translator/handlers/window/AboutHandler.java
@@ -6,6 +6,7 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipselabs.e4.tapiji.translator.views.dialogs.AboutDialog;
 
 
 public class AboutHandler {
@@ -15,6 +16,6 @@ public class AboutHandler {
     @Execute
     public void execute(final @Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
         System.out.println("Execute: " + TAG);
-        MessageDialog.openInformation(shell, "About", "e4 Application example.");
+        AboutDialog.create(shell).open();
     }
 }
