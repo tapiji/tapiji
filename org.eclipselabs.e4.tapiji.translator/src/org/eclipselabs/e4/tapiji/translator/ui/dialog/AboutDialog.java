d5fe809b85ac904134669e1f40a5959eb2e062e1
Christian Behon <christian.behon@gmail.com>
2015-05-17 18:58:08 +0200

    Add new stuff

--- org.eclipselabs.e4.tapiji.translator/src/org/eclipselabs/e4/tapiji/translator/ui/dialog/AboutDialog.java
+++ org.eclipselabs.e4.tapiji.translator/src/org/eclipselabs/e4/tapiji/translator/ui/dialog/AboutDialog.java
@@ -4,7 +4,6 @@ package org.eclipselabs.e4.tapiji.translator.ui.dialog;
 import org.eclipse.e4.tools.services.IResourcePool;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CBanner;
-import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Button;
@@ -15,13 +14,14 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipselabs.e4.tapiji.resource.TapijiResourceProvider;
 
 
-public class AboutDialog extends Dialog implements SelectionListener {
+@SuppressWarnings("restriction")
+public final class AboutDialog extends Dialog implements SelectionListener {
 
     private Shell shell;
-    private Button btnNewButton;
-    private IResourcePool resource;
+    private Button btnConfirm;
+    private final IResourcePool resource;
 
-    public AboutDialog(Shell parent, IResourcePool resource) {
+    public AboutDialog(final Shell parent, final IResourcePool resource) {
         super(parent);
         this.resource = resource;
         createContents();
@@ -31,7 +31,7 @@ public class AboutDialog extends Dialog implements SelectionListener {
         createContents();
         shell.open();
         shell.layout();
-        Display display = getParent().getDisplay();
+        final Display display = getParent().getDisplay();
         while (!shell.isDisposed()) {
             if (!display.readAndDispatch()) {
                 display.sleep();
@@ -45,54 +45,43 @@ public class AboutDialog extends Dialog implements SelectionListener {
         shell.setSize(450, 236);
         shell.setText(getText());
 
-        CBanner banner = new CBanner(shell, SWT.NONE);
+        final CBanner banner = new CBanner(shell, SWT.NONE);
         banner.setBounds(130, 75, 0, 0);
 
-        btnNewButton = new Button(shell, SWT.NONE);
-        btnNewButton.setBounds(355, 170, 83, 29);
-        btnNewButton.addSelectionListener(this);
-        btnNewButton.setData("btn_ok");
-        btnNewButton.setText("OK");
+        btnConfirm = new Button(shell, SWT.NONE);
+        btnConfirm.setBounds(355, 170, 83, 29);
+        btnConfirm.addSelectionListener(this);
+        btnConfirm.setData("btn_ok");
+        btnConfirm.setText("OK");
 
-        Button btnNewButton_1 = new Button(shell, SWT.NONE);
-        btnNewButton_1.addSelectionListener(new SelectionAdapter() {
-
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-            }
-        });
-        btnNewButton_1.setBounds(255, 170, 83, 29);
-        btnNewButton_1.setText("License");
-
-        Label lblNewLabel = new Label(shell, SWT.NONE);
+        final Label lblNewLabel = new Label(shell, SWT.NONE);
         lblNewLabel.setImage(resource.getImageUnchecked(TapijiResourceProvider.IMG_TAPIJI_LOGO_128));
         lblNewLabel.setBounds(10, 10, 138, 135);
 
-        Label lblTapijiTranslator = new Label(shell, SWT.NONE);
-        //lblTapijiTranslator.setFont(SWTResourceManager.getFont("Sans", 12, SWT.NORMAL));
+        final Label lblTapijiTranslator = new Label(shell, SWT.NONE);
         lblTapijiTranslator.setBounds(154, 24, 284, 30);
         lblTapijiTranslator.setText("Tapiji - Translator");
 
-        Label lblVersion = new Label(shell, SWT.NONE);
+        final Label lblVersion = new Label(shell, SWT.NONE);
         lblVersion.setBounds(154, 60, 284, 15);
         lblVersion.setText("Version: 1.0.0");
 
-        Label lblStefanStroblMartin = new Label(shell, SWT.NONE);
+        final Label lblStefanStroblMartin = new Label(shell, SWT.NONE);
         lblStefanStroblMartin.setBounds(154, 82, 284, 63);
         lblStefanStroblMartin.setText("Stefan Strobl, Martin Reiterer, Christian Behon");
 
     }
 
     @Override
-    public void widgetSelected(SelectionEvent e) {
-        Button btn = ((Button) e.widget);
+    public void widgetSelected(final SelectionEvent e) {
+        final Button btn = ((Button) e.widget);
         if (btn.getData().equals("btn_ok")) {
             shell.close();
         }
     }
 
     @Override
-    public void widgetDefaultSelected(SelectionEvent e) {
+    public void widgetDefaultSelected(final SelectionEvent e) {
         // TODO Auto-generated method stub
 
     }
