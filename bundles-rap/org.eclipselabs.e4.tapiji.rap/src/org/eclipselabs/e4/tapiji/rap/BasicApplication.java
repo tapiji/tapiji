445192c9536a5a2889f14cb5566f121495551317
nucle <christian.behon@rise-world.com>
2016-11-20 19:36:22 +0100

    Add single project for RAP/RCP
    * New rap project
    * New rap target project

---
+++ bundles-rap/org.eclipselabs.e4.tapiji.rap/src/org/eclipselabs/e4/tapiji/rap/BasicApplication.java
@@ -0,0 +1,40 @@
+package org.eclipselabs.e4.tapiji.rap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.e4.E4ApplicationConfig;
+import org.eclipse.rap.e4.E4EntryPointFactory;
+import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.application.ExceptionHandler;
+import org.eclipse.rap.rwt.application.Application.OperationMode;
+import org.eclipse.rap.rwt.client.WebClient;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+
+public class BasicApplication implements ApplicationConfiguration {
+
+    public void configure(Application application) {
+    	
+    	application.addStyleSheet("tapiji.business", "theme/business/business.css");
+		Map<String, String> properties = new HashMap<String, String>();
+
+		
+		properties.put(WebClient.PAGE_TITLE, "e4 RAP App");
+		properties.put(WebClient.THEME_ID, "tapiji.business");
+        application.addEntryPoint("/rap", new E4EntryPointFactory(E4ApplicationConfig.create("org.eclipselabs.e4.tapiji/Application.e4xmi")), properties);
+        application.setOperationMode( OperationMode.SWT_COMPATIBILITY );
+        
+        application.setExceptionHandler(new ExceptionHandler() {
+			
+			@Override
+			public void handleException(Throwable throwable) {
+				System.out.println("" + throwable.getMessage());
+				
+			}
+		});
+    }
+
+}
---
+++ bundles/org.eclipselabs.e4.tapiji/src/org/eclipselabs/e4/tapiji/ExitHandler.java
@@ -0,0 +1,14 @@
+package org.eclipselabs.e4.tapiji;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class ExitHandler
+{
+
+	@Execute
+	public void execute()
+	{
+		
+	}
+
+}
