512510ac346713e4508cc6b1cd155e6c02d0bc5e
Christian Behon <christian.behon@knusperleicht.at>
2017-03-18 12:47:04 +0100

    Add pull error dialogs refs #32

--- bundles/org.eclipse.e4.tapiji.git/src/org/eclipse/e4/tapiji/git/ui/handler/trimmbar/PullHandler.java
+++ bundles/org.eclipse.e4.tapiji.git/src/org/eclipse/e4/tapiji/git/ui/handler/trimmbar/PullHandler.java
@@ -1,7 +1,6 @@
 package org.eclipse.e4.tapiji.git.ui.handler.trimmbar;
 
 
-import javax.inject.Inject;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.tapiji.git.core.api.IGitService;
@@ -11,7 +10,9 @@ import org.eclipse.e4.tapiji.git.model.exception.GitServiceException;
 import org.eclipse.e4.tapiji.git.ui.preference.Preferences;
 import org.eclipse.e4.tapiji.logger.Log;
 import org.eclipse.e4.tapiji.mylyn.core.api.IMylynService;
+import org.eclipse.e4.tapiji.mylyn.model.Notification;
 import org.eclipse.e4.ui.di.UISynchronize;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
 import org.eclipse.swt.widgets.Shell;
 
@@ -20,18 +21,14 @@ public class PullHandler {
 
     private static final String TAG = PullHandler.class.getSimpleName();
 
-    @Inject
-    UISynchronize sync;
-
     @Execute
-    public void execute(final Shell shell, final IGitService service, final IMylynService mylyn) {
+    public void execute(final Shell shell, final IGitService service, final IMylynService mylyn, UISynchronize sync) {
         Log.d(TAG, "PullHandler execute()");
         service.pullFastForward(new IGitServiceCallback<Void>() {
 
             @Override
             public void onSuccess(GitServiceResult<Void> response) {
-                // TODO Auto-generated method stub
-
+                sync.asyncExec(() -> mylyn.sendNotification(new Notification("Pull Successful", "")));
             }
 
             @Override
@@ -40,21 +37,12 @@ public class PullHandler {
                 if (exception.getCause() instanceof WrongRepositoryStateException) {
                     Log.d(TAG, "PullHandler execute()" + (exception.getCause()));
                     if (exception.getCause().getMessage().contains("MERGING")) {
-                        service.mergeStatus(new IGitServiceCallback<Void>() {
-
-                            @Override
-                            public void onSuccess(GitServiceResult<Void> response) {
-                                // TODO Auto-generated method stub
-
-                            }
-
-                            @Override
-                            public void onError(GitServiceException exception) {
-                                // TODO Auto-generated method stub
-
-                            }
-                        });
+                        sync.asyncExec(() -> MessageDialog.openError(shell, "Error Pull", "Cannot pull into a repository with state merge."));
+                    } else {
+                        sync.asyncExec(() -> MessageDialog.openError(shell, "Error: ", exception.getMessage()));
                     }
+                } else {
+                    sync.asyncExec(() -> MessageDialog.openError(shell, "Error: ", exception.getMessage()));
                 }
             }
         });
